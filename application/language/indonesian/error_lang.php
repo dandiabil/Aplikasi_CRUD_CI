a957e9cc1e30b5002cf304d981627664c6aa7ed1
dandiabil <dandiabil24@gmail.com>
2020-04-03 15:34:03 +0700

    Menambahkan file ke git

---
+++ application/language/indonesian/error_lang.php
@@ -0,0 +1,4 @@
+<?php
+    $lang['error_savedata'] = "Tidak dapat menyimpan data ke Server.";
+    $lang['error_updatedata'] = "Tidak dapat memperbaiki data ke Server.";
+?>
---
+++ system/language/english/error_lang.php
@@ -0,0 +1,4 @@
+<?php
+    $lang['error_savedata'] = "Can't save data on Sever.";
+    $lang['error_updatedata'] = "Can't update data on Server.";
+?>
