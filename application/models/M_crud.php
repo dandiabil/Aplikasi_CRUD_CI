f00587fb3b7f506d0ccba0dd9c11c4d8641464ee
dandiabil <dandiabil24@gmail.com>
2020-04-08 20:08:07 +0700

    menambahkan fungsi di file model M_crud

--- application/models/M_crud.php
+++ application/models/M_crud.php
@@ -3,6 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_crud extends CI_Model{
 
+    public function get_all(){
+        $this->db->select('*');
+        $this->db->from('tbl_barang');
+        $this->db->order_by('id_barang', 'DESC');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+    
+    public function simpan_data($data){
+        $query = $this->db->insert('tbl_barang', $data);
+        if($query){
+            return true;
+        }else return false;
+    }
+
     
 }
 
