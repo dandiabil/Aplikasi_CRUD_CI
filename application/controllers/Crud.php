eed2918bb631a57ffd95609548c839f9dacbbb5a
dandiabil <dandiabil24@gmail.com>
2020-04-08 19:46:05 +0700

    mengganti crud controller

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -5,16 +5,10 @@ class Crud extends CI_Controller{
 
     public function _construct(){
         parent::_construct();
-        $this->load->database();
-        $this->load->helper('url_helper');
         $this->load->model('M_crud');
-        $this->load->helper('form');
-        $this->load->library('form_validation');
     }
 
-    public function index(){
-        
-    }
+    
 }
 
 
