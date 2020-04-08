fb5f119d23555221a99068fbd3a485b3032907ca
dandiabil <dandiabil24@gmail.com>
2020-04-08 19:15:46 +0700

    Mengubah tampilan welcome_message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Aplikasi CRUD</title>
 
 	<style type="text/css">
 
@@ -20,12 +20,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}
 
 	a {
-		color: #003399;
-		background-color: transparent;
+		color: black;
+		background-color: #E0D8D6;
+		border-radius: 5px;
 		font-weight: normal;
+		padding : 1px 5px;
+		font-size: 20px;
+		text-decoration: none;
 	}
 
-	h1 {
+	h3 {
 		color: #444;
 		background-color: transparent;
 		border-bottom: 1px solid #D0D0D0;
@@ -35,24 +39,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		padding: 14px 15px 10px 15px;
 	}
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
 	#body {
 		margin: 0 15px 0 15px;
 	}
 
 	p.footer {
 		text-align: right;
-		font-size: 11px;
+		font-size: 9px;
 		border-top: 1px solid #D0D0D0;
 		line-height: 32px;
 		padding: 0 10px 0 10px;
@@ -60,10 +53,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}
 
 	#container {
-		margin: 10px;
+		padding : 30px 20px;
+		margin: 80px auto;
+		width : 350px;
 		border: 1px solid #D0D0D0;
 		box-shadow: 0 0 8px #D0D0D0;
 	}
+
+	
 	</style>
 </head>
 <body>
@@ -72,8 +69,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div id="body">
 		<h3>Aplikasi CRUD</h3>
-		<a href="#">Tambah Data</a>
-		<a href="#">Lihat Daftar Data</a>
+		<p><i>Create, Read, Update and Delete</i></p>
+		<a href="#" class="tambah">Tambah Data</a>
+		<br><br>
+		<a href="#" class="daftar">Daftar Data</a>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
