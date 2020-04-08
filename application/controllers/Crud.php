65453ad96645c5b4feaf5ddf1a5fc5986d43d55b
dandiabil <dandiabil24@gmail.com>
2020-04-08 19:46:36 +0700

    menambahkan index

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -8,7 +8,15 @@ class Crud extends CI_Controller{
         $this->load->model('M_crud');
     }
 
-    
+    public function index(){
+        $data = array(
+            'title' => 'Data Barang',
+            'data_barang' => $this->M_crud->getAll(),
+        );
+        $this->load->view('data_barang', $data);
+    }
+
+
 }
 
 
