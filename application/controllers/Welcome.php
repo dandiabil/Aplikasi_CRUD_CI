504472e7caafb34a534cf063d9628fc06cefae3b
dandiabil <dandiabil24@gmail.com>
2020-04-11 17:43:30 +0700

    ngubah file

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,11 +18,6 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function __construct(){
-		parent::__construct();
-        $this->load->model('M_crud');
-    
-	}
 	
 	 public function index()
 	{
