45dbecc9982b87fd758d599a034bb8b96f23f24e
dandiabil <dandiabil24@gmail.com>
2020-04-11 19:09:11 +0700

    merubah link di view edit

---
+++ application/views/v_edit.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title ?></title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 9px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		padding : 30px 20px;
+		margin: 80px auto;
+		width : 350px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+
+	
+	</style>
+</head>
+<body>
+
+<div id="container">
+
+	<div id="body">
+		<?php echo form_open(crud/update_data) ?>
+
+        <label for="id_barang">ID Barang</label>
+        <input type="text" name="id_barang" placeholder="Masukkan ID Barang" value="<?php echo $v_tampildata->id_barang ?>">
+
+        <label for="nama_barang">Nama Barang</label>
+        <input type="text" name="nama_barang" placeholder="Masukkan Nama Barang" value="<?php echo $v_tampildata->nama_barang ?>">
+
+        <label for="jenis_barang">Jenis Barang</label>
+        <input type="text" name="jenis_barang" placeholder="Masukkan Jenis Barang" value="<?php echo $v_tampildata->jenis_barang ?>">
+
+        <label for="harga">Harga Barang</label>
+        <input type="text" name="harga" value="<?php echo $v_tampildata->harga ?>">
+
+        <label for="stok">Stok Barang</label>
+        <input type="text" name="stok" value="<?php echo $v_tampildata->stok ?>">
+
+        <button type="submit">Simpan</button>
+        <button type="reset">Reset</button>
+
+		<?php echo form_close() ?>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+</body>
+</html>
