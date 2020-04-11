16634d8e494f1da3052f733d2f0544bdf565e606
dandiabil <dandiabil24@gmail.com>
2020-04-11 18:47:05 +0700

    merubah file v_create

--- application/views/v_create.php
+++ application/views/v_create.php
@@ -68,6 +68,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         <button type="submit">Simpan</button>
         <button type="reset">Reset</button>
+
+		<?php form_close() ?>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
