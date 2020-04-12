37968bc71bd019dbdad600fa2bd5c669c52579ae
dandiabil <dandiabil24@gmail.com>
2020-04-12 09:50:45 +0700

    ngubah file controller

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -6,13 +6,13 @@ class Crud extends CI_Controller{
     public function __construct(){
         parent::__construct();
         $this->load->model('M_crud');
-        $this->load->database();
-        $this->load->helper('url_helper');
-        $this->load->helper('form');
-        $this->load->library('form_validation');
     }
 
     public function index(){
+        redirect('Crud/Home');
+    }
+
+    public function Home(){
         $this->load->view('v_home');
     }
 
@@ -27,13 +27,14 @@ class Crud extends CI_Controller{
 
     public function tambah_data(){
         $data = array(
-            'title' => 'Tambah Data Barang',
+            'title' => 'Tambah Data Barang'
         );
         $this->load->view('v_create', $data);
     }
 
     public function simpan_data(){
         $data = array(
+            'id_barang' => $this->input->post("id_barang"),
             'nama_barang' => $this->input->post("nama_barang"),
             'jenis_barang' => $this->input->post("jenis_barang"),
             'harga' => $this->input->post("harga"),
@@ -41,8 +42,7 @@ class Crud extends CI_Controller{
         );
 
         $this->M_crud->simpan_data($data);
-
-        redirect('Crud/');
+        redirect('Crud/tampil_data');
     }
 
     public function edit_data($id_barang){
@@ -50,7 +50,7 @@ class Crud extends CI_Controller{
 
         $data = array(
             'title' => 'Edit Data',
-            'data_barang' => $this->M_crud->edit_data($id_barang),
+            'v_tampildata' => $this->M_crud->edit_data($id_barang),
         );
         $this->load->view('v_edit', $data);
     }
@@ -64,15 +64,13 @@ class Crud extends CI_Controller{
             'stok' => $this->input->post("stok"),
         );
         $this->M_crud->update_data($data, $id);
+        redirect('Crud/tampil_data');
     }
 
     public function delete_data($id_barang){
         $id['id_barang'] = $this->uri->segment(3);
         $this->M_crud->delete_data($id);
-
-        
-
-        redirect('Crud/');
+        redirect('Crud/tampil_data');
     }
 
 }
